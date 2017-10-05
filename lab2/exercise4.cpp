ee8a27097e97a6119d458bc9e7eb10b5909e865c
William Anderson <carrig212@gmail.com>
2017-10-05 12:02:30 +0100

    Added incomplete solution to exercise 4

--- lab2/exercise4.cpp
+++ lab2/exercise4.cpp
@@ -2,9 +2,37 @@
 
 using namespace std;
 
-int main()
+const int size = 50;
+
+
+int my_string_len(char str[])
+{
+	int count = 0;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	return count;
+}
+
+void my_string_cat(char dest[], char src[], int dest_size)
 {
 	
+}
+
+int main()
+{
+	char src[size], dest[size];
+
+	cout << "Enter a source string less than 50 characters long:" << endl;
+	cin.getline(src, size);
+
+	cout << "Enter a destination string less than 50 characters long:" << endl;
+	cin.getline(dest, size);
+
+	my_string_cat(dest, src, size);
 
 	return 0;
 }
