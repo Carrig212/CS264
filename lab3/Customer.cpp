46f991135a911890780c9c0fa17dfff75884e003
William Anderson <carrig212@gmail.com>
2017-10-13 12:06:56 +0100

    Add exercise 6

--- lab3/Customer.cpp
+++ lab3/Customer.cpp
@@ -3,8 +3,41 @@
 
 using namespace std;
 
-void printCustomers(Customer &head) {
-    Customer* current = &head;
+Customer* createList(string name) {
+    Customer* customer = new Customer();
+    customer -> name = name;
+    customer -> next = NULL;
+
+    return customer;
+}
+
+void insertName(Customer* head, string name) {
+    Customer* current = head;
+
+    while (current -> next != NULL) {
+        current = current -> next;
+    }
+
+    current -> next = new Customer();
+    current = current -> next;
+    current -> name = name;
+    current -> next = NULL;
+}
+
+int listLength(Customer* head) {
+    Customer* current = head;
+    int count = 0;
+
+    while (current != NULL) {
+        current = current -> next;
+        count++;
+    }
+
+    return count;
+}
+
+void printCustomers(Customer *head) {
+    Customer* current = head;
 
     while (current != NULL) {
         cout << current -> name << endl;
--- lab3/Customer.h
+++ lab3/Customer.h
@@ -7,4 +7,44 @@ struct Customer {
     Customer* next;
 };
 
-void printCustomers(Customer &head);
+Customer* createList(string name) {
+    Customer* customer = new Customer();
+    customer -> name = name;
+    customer -> next = NULL;
+
+    return customer;
+}
+
+void insertName(Customer* head, string name) {
+    Customer* current = head;
+
+    while (current -> next != NULL) {
+        current = current -> next;
+    }
+
+    current -> next = new Customer();
+    current = current -> next;
+    current -> name = name;
+    current -> next = NULL;
+}
+
+int listLength(Customer* head) {
+    Customer* current = head;
+    int count = 0;
+
+    while (current != NULL) {
+        current = current -> next;
+        count++;
+    }
+
+    return count;
+}
+
+void printCustomers(Customer *head) {
+    Customer* current = head;
+
+    while (current != NULL) {
+        cout << current -> name << endl;
+        current = current -> next;
+    }
+}
