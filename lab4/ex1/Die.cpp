9afe01d50f8ea7f520aec95eecef155fd22a3443
William Anderson <carrig212@gmail.com>
2017-10-30 11:32:21 +0000

    Fix exercise 1 to default to 6 sides

--- lab4/ex1/Die.cpp
+++ lab4/ex1/Die.cpp
@@ -10,19 +10,18 @@ using namespace std;
 Die::Die(int n) {
     srand(time(NULL));
 
-    while (n < 4) {
-        cout << "Invalid number of sides: number of sides must be four or more." << endl;
-        cout << "Try again: ";
-        cin >> n;
+    if (n < 4) {
+        cout << "Invalid number of sides: defaulting to 6." << endl;
+        sides = 6;
+    } else {
+        sides = n;
     }
 
-    sides = n;
     value = 0;
 }
 
 int Die::roll() {
     value = (rand() % sides) + 1;
-
     return value;
 }
 
