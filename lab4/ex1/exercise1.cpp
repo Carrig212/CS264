3840987621a436ea3d69f9b4030cbee30c52bfa2
WILLIAM STEVEN ANDERSON <william.anderson.2016@mumail.ie>
2017-11-03 13:17:11 +0000

    fix ex1/exercise1 to use two dice

--- lab4/ex1/exercise1.cpp
+++ lab4/ex1/exercise1.cpp
@@ -9,17 +9,19 @@ int main() {
     int n;
     int sides;
 
-    cout << "Enter the number of sides you'd like the die to have: ";
+    cout << "Enter the number of sides you'd like the dice to have: ";
     cin >> sides;
 
-    Die die(sides);
-    double freq[sides];
+    Die die1(sides);
+    Die die2(sides);
 
-    for (int i = 0; i < sides; i++) {
+    double freq[sides * 2];
+
+    for (int i = 0; i < sides * 2; i++) {
         freq[i] = 0;
     }
 
-    cout << "Enter the number of times you'd like to roll the die: ";
+    cout << "Enter the number of times you'd like to roll the dice: ";
     cin >> n;
 
     if (n < 1) {
@@ -27,12 +29,13 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        die.roll();
-        freq[die.getValue() - 1]++;
+        die1.roll();
+        die2.roll();
+        freq[die1.getValue() + die2.getValue() - 2]++;
     }
 
-    for (int i = 0; i < die.getNumsides(); i++) {
-        cout << (i + 1) << " = " << (freq[i] / (n * 1.0)) * 100 << "%" << endl;
+    for (int i = 0; i < sides * 2 - 1; i++) {
+        cout << (i + 2) << " = " << (freq[i] / (n * 1.0)) * 100 << "%" << endl;
     }
 
     return 0;
